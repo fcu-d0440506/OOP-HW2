0dadc239022d512b8a69686f704da85c0abeed9f
er860511 <er860511@DESKTOP-CPRQNVT>
2016-10-11 23:33:43 +0800

    hhw

--- src/PrintNumberInWord.java
+++ src/PrintNumberInWord.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class PrintNumberInWord {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  Scanner scanner = new Scanner(System.in);
+		  System.out.println("please enter a number:");
+		  int a = input.nextInt();
+		  if(a>0&&a<10)
+		  {
+			  int b = a + 96;
+			  System.out.printf("%d\n",b);
+		  }
+		  else
+		  {
+			  System.out.println("other\n");
+		  }
+		 
 
 	}
 
